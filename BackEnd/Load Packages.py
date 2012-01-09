eebfa186f11271d24361341da1c1790ec2a7f7cd
System Administrator <root@iPhone.(none)>
2012-01-08 23:42:32 -0600

    Added A POC script to display the packages by name

--- BackEnd/Load Packages.py
+++ BackEnd/Load Packages.py
@@ -2,40 +2,37 @@
 
 import os
 import glob
+import shelve
 
-# A whole bunch of defines
-MasterPackages=shelve.open(myfile,)
+MasterPackages=shelve.open('../Cache/MasterPackages', writeback=True)
 
 Tags=['Package', 'Name', 'Section', 'Description']
 
 path = '/var/lib/apt/lists'
 for packagesfile in glob.glob( os.path.join(path, '*_Packages') ):
-    with open(packages) as reader:
-        for line in reader:
-            line=line.strip()
-            if line == '':
-                if LastTag != "Description":
-                    if PkgInfo.has_key('Package'):
-                        PackageID=PkgInfo['Package']
-                        del PkgInfo['Package']
-                        if MasterPackages.has_key(PackageID): # TODO:
-                            pass # check version number to find out which is newer
-                        MasterPackages[PackageID]=PkgInfo
-                        PkgInfo={}
-            elif len(line.split(': ',1)) == 2:
-                line=line.split(':', 1)
-                if line[0] in Tags:
-                    LastTag=line[0]
-                    PkgInfo[line[0]]=line[1]
-            else:
-                if line.startswith(' '):
-                    PkgInfo['Description']=PkgInfo[Description]+line+"\n"
-raw_input("All Packages Loaded, press enter to view")
+    reader = open(packagesfile)
+    PkgInfo={}
+    for line in reader.readlines():
+        line=line.strip()
+        if line == '':
+            if LastTag != "Description":
+                if PkgInfo.has_key('Package'):
+                    PackageID=PkgInfo['Package']
+                    del PkgInfo['Package']
+                    if MasterPackages.has_key(PackageID): # TODO:
+                        pass # check version number to find out which is newer
+                    MasterPackages[PackageID]=PkgInfo
+                    PkgInfo={}
+        elif len(line.split(': ',1)) == 2:
+            line=line.split(':', 1)
+            if line[0] in Tags:
+                LastTag=line[0]
+                PkgInfo[line[0]]=line[1]
+        else:
+            if line.startswith(' '):
+                PkgInfo['Description']=PkgInfo[Description]+line+"\n"
+#raw_input("All Packages Loaded, press enter to view")
 writer = open("output",'w')
 writer.write(str(MasterPackages))
 writer.close()
-
-print "<table>"
-for PID in MasterPackages:
-    print "<tr><td>", MasterPackages[PID["Name"]], "</td></tr>"
-print "</table>"
+#print MasterPackages
